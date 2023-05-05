715d6c8fbee58d1f9c09a1c20d0a7cd807d426c8
Tosin Ojo <ojotosin1900@gmail.com>
2023-05-05 12:05:43 -0400

    adding server bootstrap file

---
+++ server-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo su
+yum update -y
+yum install -y httpd
+cd /var/www/html
+wget https://github.com/ojotosin/jupiter-github-action-pipeline/raw/main/jupiter-main.zip
+unzip main.zip
+cp -r jupiter-main/* /var/www/html/
+rm -rf jupiter-main main.zip
+systemctl enable httpd 
+systemctl start httpd
